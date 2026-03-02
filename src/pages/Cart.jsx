4fe032fac0eb81830cfa584e851b8fef2c825217
Sidhant001 <143825824+Sidhant001@users.noreply.github.com>
2026-03-02 13:38:40 +0530

    Added Remove and Add buttons to Cart page, and a Clear Cart button. Updated styling for better UX.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeFromCart } from "../features/cartSlice";
+import { removeFromCart , addToCart ,clearCart} from "../features/cartSlice";
 import {useNavigate} from "react-router-dom"
 function Cart() {
   const dispatch = useDispatch();
@@ -22,12 +22,24 @@ function Cart() {
             >
               <span>{item.name} (x{item.quantity})</span>
               <span>₹{item.price}</span>
-              <button
-                onClick={() => dispatch(removeFromCart(item.id))}
-                className="bg-red-600 px-3 py-1 rounded"
-              >
-                Remove
-              </button>
+                       <div className="flex items-center gap-3">
+      <button
+        onClick={() => dispatch(removeFromCart(item.id))}
+        className="bg-red-600 px-3 py-1 rounded"
+      >
+        -
+      </button>
+      <button
+        onClick={() =>
+          dispatch(addToCart(item))
+        }
+        className="bg-green-600 px-3 py-1 rounded"
+      >
+        +
+      </button>
+           </div>
+
+          <span>₹{item.price * item.quantity}</span>
             </div>
           ))}
 
@@ -39,6 +51,10 @@ function Cart() {
           className="mt-5 bg-blue-600 px-4 py-2 rounded hover:bg-blue-700">
             Checkout
             </button>
+            <button
+            onClick={()=>dispatch(clearCart())}
+            className="absolute right-9 bg-red-600 px-4 py-2 rounded  hover:bg-red-700"
+            >Remove</button>
           
         </>
       )}
